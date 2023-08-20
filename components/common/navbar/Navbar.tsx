e681af56baeaa9a062edf31105d511d42d988d68
GoodGuyForReal <tdoganay325@gmail.com>
2023-08-20 11:37:42 +0200

    Typograph added

--- components/common/navbar/Navbar.tsx
+++ components/common/navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import ActiveUserNavbar from "./ActiveUserNavbar/ActiveUserNavbar";
 import InActiveUserNavbar from "./InActiveUserNavbar/InActiveUserNavbar";
 import { supabaseClient } from "@/db/supabaseClient";
 import { User } from "@/types/Types";
+import Typography from "@/Typography/Typography";
+
 
 export default async function Navbar() {
   const sessionUserID = "982ad381-1060-498d-8782-d78b26d4f979";
@@ -19,7 +21,7 @@ export default async function Navbar() {
     <div className="flex h-28 w-full items-center justify-between py-4">
       <div>
         <Link href={"/"} className="font-semibold">
-          dis·cus·sion
+          <Typography text="dis·cus·sion" tagName="p" variation="default" />
         </Link>
       </div>
       {data ? <ActiveUserNavbar user={user} /> : <InActiveUserNavbar />}
