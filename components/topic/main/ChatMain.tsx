e279d2791597180a0f232b1c758de41a5485d4df
GoodGuyForReal <tdoganay325@gmail.com>
2023-09-21 14:16:04 +0200

    chat section created

--- components/topic/main/ChatMain.tsx
+++ components/topic/main/ChatMain.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import ChatHeader from "./_components/ChatHeader";
+import ChatMainScreen from "./_components/ChatMainScreen";
+import SendMessage from "./_components/SendMessage";
 
 export default function ChatMain({ topicID }: { topicID: string }) {
-  return <div className="h-[800px] w-full rounded-md bg-slate-600">{topicID}</div>;
+  return (
+    <div className="h-screen w-full rounded-md bg-slate-600">
+      <ChatHeader />
+      <ChatMainScreen />
+      <SendMessage />
+    </div>
+  );
 }
