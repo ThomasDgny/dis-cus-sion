157fdc846a9160958393276a8166a7c4cc905164
GoodGuyForReal <tdoganay325@gmail.com>
2023-10-10 00:16:04 +0200

    category json updated

--- mock/Category.ts
+++ mock/Category.ts
@@ -1,7 +1,7 @@
 interface Category {
   id: number;
   category_name: string;
-  value: string; // Add a 'value' property
+  value: string;
 }
 
 export const category: Category[] = [
@@ -12,23 +12,23 @@ export const category: Category[] = [
   },
   {
     id: 1,
+    category_name: "Game",
+    value: "Game",
+  },
+  {
+    id: 2,
     category_name: "Technology",
     value: "Technology",
   },
   {
-    id: 2,
+    id: 3,
     category_name: "Literature",
     value: "Literature",
   },
   {
-    id: 3,
+    id: 4,
     category_name: "Nature",
     value: "Nature",
-  },
-  {
-    id: 4,
-    category_name: "Food & Cooking",
-    value: "FoodAndCooking",
   },
   {
     id: 5,
@@ -52,122 +52,42 @@ export const category: Category[] = [
   },
   {
     id: 9,
-    category_name: "Art & Crafts",
-    value: "ArtAndCrafts",
-  },
-  {
-    id: 10,
     category_name: "Culture",
     value: "Culture",
   },
   {
-    id: 11,
-    category_name: "Adventure Sports",
-    value: "AdventureSports",
-  },
-  {
-    id: 12,
+    id: 10,
     category_name: "Art",
     value: "Art",
   },
   {
-    id: 13,
-    category_name: "Nature & Science",
-    value: "NatureAndScience",
-  },
-  {
-    id: 14,
-    category_name: "Home & Gardening",
-    value: "HomeAndGardening",
-  },
-  {
-    id: 15,
+    id: 11,
     category_name: "Writing",
     value: "Writing",
   },
   {
-    id: 16,
-    category_name: "Fashion & Sustainability",
-    value: "FashionAndSustainability",
-  },
-  {
-    id: 17,
-    category_name: "Music & Culture",
-    value: "MusicAndCulture",
-  },
-  {
-    id: 18,
-    category_name: "Books & Fantasy",
-    value: "BooksAndFantasy",
-  },
-  {
-    id: 19,
-    category_name: "Culture & Diversity",
-    value: "CultureAndDiversity",
-  },
-  {
-    id: 20,
-    category_name: "Photography & Nature",
-    value: "Photography&Nature",
-  },
-  {
-    id: 21,
-    category_name: "Science & Astronomy",
-    value: "ScienceAndAstronomy",
-  },
-  {
-    id: 22,
-    category_name: "Travel & Lifestyle",
-    value: "TravelAndLifestyle",
-  },
-  {
-    id: 23,
-    category_name: "Art & Creativity",
-    value: "ArtAndCreativity",
-  },
-  {
-    id: 24,
+    id: 12,
     category_name: "Photography",
     value: "Photography",
   },
   {
-    id: 25,
-    category_name: "Food & Dining",
-    value: "FoodAndDining",
-  },
-  {
-    id: 26,
+    id: 13,
     category_name: "History",
     value: "History",
   },
   {
-    id: 27,
+    id: 14,
     category_name: "Wellness",
     value: "Wellness",
   },
   {
-    id: 28,
-    category_name: "Science & Space",
-    value: "ScienceAndSpace",
-  },
-  {
-    id: 29,
-    category_name: "Food & Drinks",
-    value: "FoodAndDrinks",
-  },
-  {
-    id: 30,
+    id: 15,
     category_name: "Architecture",
     value: "Architecture",
   },
   {
-    id: 31,
+    id: 16,
     category_name: "Psychology",
     value: "Psychology",
-  },
-  {
-    id: 32,
-    category_name: "Nature & Environment",
-    value: "NatureAndEnvironment",
   },
 ];
