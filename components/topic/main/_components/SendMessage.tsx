aee193ed22bf2e6e9400b17e8ef3760777e68a03
GoodGuyForReal <tdoganay325@gmail.com>
2023-10-15 12:19:47 +0200

    send message form reset added

--- components/topic/main/_components/SendMessage.tsx
+++ components/topic/main/_components/SendMessage.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import { Input } from "@/components/ui/input";
 import { useAuth } from "@/context/AuthProvider";
 import { Button } from "@/components/ui/button";
@@ -13,18 +13,25 @@ interface SendMessageProps {
 export default function SendMessage({ topicID }: SendMessageProps) {
   const { user } = useAuth();
   const { toast } = useToast();
+  const formRef = useRef<HTMLFormElement>(null);
   if (!user) return null;
 
   const handleSubmit = async (formData: FormData) => {
     const result = await handleSendMessage(formData, user.id, topicID);
     if (result?.error) {
       toast({ variant: "destructive", description: result.error });
+    } else {
+      formRef.current?.reset();
     }
   };
 
   return (
     <div>
-      <form action={handleSubmit} className="flex gap-3 border-t p-2">
+      <form
+        ref={formRef}
+        action={handleSubmit}
+        className="flex gap-3 border-t p-2"
+      >
         <Input
           type="text"
           name="message"
